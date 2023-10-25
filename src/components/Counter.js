42d316228728ea6cd55e68732768dfed43736680
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-25 14:35:43 +0300

    Completed Assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,12 +5,13 @@ function Counter() {
 
   function increment() {
     console.log(`before setState: ${count}`);
-    setCount((currentCount) => currentCount +1);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
     console.log(`after setState: ${count}`);
 
   }
 
-  return <button onClick={increment}>{count}</button>;
+  return <div onClick={increment}>{count}</div>;
 }
 
 export default Counter;
